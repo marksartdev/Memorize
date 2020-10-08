15a4c8a2d5373db2e83b8116ad962ace5fbacbfd
Mark Sart <marksart.pro@gmail.com>
2020-10-08 10:29:24 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mark Sartakov on 08.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
