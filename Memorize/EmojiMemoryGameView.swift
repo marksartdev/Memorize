2eb9a4d988976149121a7813919312e708a69384
Mark Sart <marksart.pro@gmail.com>
2020-10-26 14:50:15 +0300

    Lesson 5: Shape

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -55,6 +55,8 @@ struct CardView: View {
                 if card.isFaceUp {
                     RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                     RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                        .padding(5).opacity(0.4)
                     Text(card.content)
                 } else {
                     if !card.isMatched {
@@ -74,12 +76,15 @@ struct CardView: View {
     private let edgeLineWidth: CGFloat = 3
     
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 struct EmojiMemoryGameView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(emojiMemoryGame: EmojiMemoryGame())
+        let emojiMemoryGame = EmojiMemoryGame()
+        emojiMemoryGame.startNewGame()
+        emojiMemoryGame.choose(card: emojiMemoryGame.cards[0])
+        return EmojiMemoryGameView(emojiMemoryGame: emojiMemoryGame)
     }
 }
